b0b758a12653bfcd29f5d3e36391b99c024d473e
Redhat Raptor <redhat.raptor@gmail.com>
2014-12-22 02:33:01 +0800

    Adds identify event for user

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
     , http = require('http')
-    , socketio = require('socket.io');
- 
+    , socketio = require('socket.io')
+    , _ = require('underscore');
+
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     var template = fs.readFileSync(__dirname + '/index.html');
@@ -9,10 +10,39 @@ var server = http.createServer(function(req, res) {
 }).listen(8080, function() {
     console.log('Tracking users at: http://localhost:8080');
 });
- 
+
+var userData = {
+    users: {},
+    getUser: function(id) {
+        return this.users[id];
+    },
+    addNewUser: function() {
+        var id = _.uniqueId('user_');
+        this.users[id] = {
+            id: id,
+            created: new Date().getTime()
+        };
+
+        return this.users[id];
+    }
+};
+
 socketio.listen(server).on('connection', function (socket) {
     socket.on('message', function (msg) {
+        if(msg.type == 'identify') {
+            var data = msg.data;
+
+            if(!data || !data.id) {
+                data = userData.addNewUser();
+            }
+
+            console.log(['udata', data]);
+            socket.emit('identify', data);
+        }
+
         console.log('Message Received: ', msg);
+        //console.log(socket.request.headers.cookie);
+
         socket.broadcast.emit('message', msg);
     });
 });
